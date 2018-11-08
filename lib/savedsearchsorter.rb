a37d1e2e50ed8f9f7726228cdb2b242ba9ff80d7
jc <jcaalve@gmail.com>
2018-11-08 00:03:39 +0000

    Start of refactoring

--- lib/savedsearchsorter.rb
+++ lib/savedsearchsorter.rb
@@ -4,148 +4,205 @@ require 'rubygems'
 require 'json'
 require 'pp'
 
+
 module Savedsearchsorter
   class Error < StandardError; end
 
-  test1 = false
-  test2 = false
-  test3 = false
-  test4 = false
-  test5 = true
 
-  #test of json parsing
-  if test1 == true
-    json = File.read('importtest.json')
-    obj = JSON.parse(json)
+  class Utilitymethods
 
-    #pp obj
+    def jsonparsetest
+      json = File.read('importtest.json')
+      obj = JSON.parse(json)
 
-    print obj[0]["_source"]["description"]
-  end
-
-  #test of directory iteration
-  if test2 == true
-    Dir.foreach('./jsons/') do |item|
-      next if item == '.' or item == '..'
-
-        #print item.pretty_inspect
-        puts item
-
-        #it doesn't appear to treat 'item' as a file object it is a just a string of the filename..
-        obj = JSON.parse(File.read('./jsons/' + item))
-        print obj[0]["_source"]["description"]
-        puts "\n"
-    end
-  end
-
-  #test of searching for user input
-  if test3 == true
-
-    puts "Please enter search term"
-    #get user input - chomp is to remove 'enter' character
-    searchquery = gets()
-    puts "Searching stored Saved Searches for asset " + searchquery
-    searchquery = searchquery.chomp.downcase
-
-    puts "============== \n"
-    puts "Results:"
-
-    Dir.foreach('./jsons/') do |item|
-      next if item == '.' or item == '..'
-
-      #puts item
-
-      #it doesn't appear to treat 'item' as a file object it is a just a string of the filename..
-      obj = JSON.parse(File.read('./jsons/' + item))
-      description = obj[0]["_source"]["description"]
-
-      if description.downcase.include? searchquery
-        puts item
-        puts obj[0]["_source"]["title"]
-        puts "\n"
-      end
-
+      print obj[0]["_source"]["description"]
     end
 
-    puts "============="
+    #test of directory iteration
+    def directorytraversaltest
+      Dir.foreach('./jsons/') do |item|
+        next if item == '.' or item == '..'
 
-  end
-
-  #test of searching from input list
-  if test4 == true
-
-    puts "============== \n"
-    puts "Results:"
-
-    File.open('assetlist.txt') do |searchlist|
-      searchlist.each_line do |searchquery|
-
-        puts "Asset: " + searchquery
-        assetname = searchquery.downcase.chomp
-
-        Dir.foreach('./jsons/') do |item|
-          next if item == '.' or item == '..'
+          #print item.pretty_inspect
+          puts item
 
           #it doesn't appear to treat 'item' as a file object it is a just a string of the filename..
           obj = JSON.parse(File.read('./jsons/' + item))
-          description = obj[0]["_source"]["description"]
-
-          if description.downcase.include? assetname
-            puts item
-            puts obj[0]["_source"]["title"]
-            puts "\n"
-          end
-
-        end
+          print obj[0]["_source"]["description"]
+          puts "\n"
       end
     end
 
-    puts "============="
+    #test of searching for user input
+    def userinputtest
+      puts "Please enter search term"
+      #get user input - chomp is to remove 'enter' character
+      searchquery = gets()
+      puts "Searching stored Saved Searches for asset " + searchquery
+      searchquery = searchquery.chomp.downcase
 
-  end
+      puts "============== \n"
+      puts "Results:"
 
-  ##searches multiple json entries from single json file using asset list as input
-  if test5 == true
+      Dir.foreach('./jsons/') do |item|
+        next if item == '.' or item == '..'
 
-    puts "============== \n"
-    puts "Results:"
+        #puts item
 
-    File.open('assetlist.txt') do |searchlist|
-      searchlist.each_line do |searchquery|
+        #it doesn't appear to treat 'item' as a file object it is a just a string of the filename..
+        obj = JSON.parse(File.read('./jsons/' + item))
+        description = obj[0]["_source"]["description"]
 
-        puts "Asset: " + searchquery
-        assetname = searchquery.downcase.chomp
-        noresults = true
-
-       obj = JSON.parse(File.read('multiple.json'))
-
-        #iterate through json array
-        obj.each do |element|
-
-          description = element["_source"]["description"]
-
-          if description.downcase.include? assetname
-            noresults = false
-            puts element["_source"]["title"]
-            puts "\n"
-
-          end
-
-        end
-
-        if noresults == true
-          puts "No Results"
+        if description.downcase.include? searchquery
+          puts item
+          puts obj[0]["_source"]["title"]
           puts "\n"
         end
 
       end
 
+      puts "============="
+
+    end
+
+    #test of searching from input list
+    def inputlisttest
+      puts "============== \n"
+      puts "Results:"
+
+      File.open('assetlist.txt') do |searchlist|
+        searchlist.each_line do |searchquery|
+
+          puts "Asset: " + searchquery
+          assetname = searchquery.downcase.chomp
+
+          Dir.foreach('./jsons/') do |item|
+            next if item == '.' or item == '..'
+
+            #it doesn't appear to treat 'item' as a file object it is a just a string of the filename..
+            obj = JSON.parse(File.read('./jsons/' + item))
+            description = obj[0]["_source"]["description"]
+
+            if description.downcase.include? assetname
+              puts item
+              puts obj[0]["_source"]["title"]
+              puts "\n"
+            end
+
+          end
+        end
+      end
+
+      puts "============="
+
+    end
+
+    ##searches multiple json entries from single json file using asset list as input
+    def searchmultipletest
+      puts "============== \n"
+      puts "Results:"
+
+      File.open('assetlist.txt') do |searchlist|
+        searchlist.each_line do |searchquery|
+
+          puts "Asset: " + searchquery
+          assetname = searchquery.downcase.chomp
+          noresults = true
+
+         obj = JSON.parse(File.read('multiple.json'))
+
+          #iterate through json array
+          obj.each do |element|
+
+            description = element["_source"]["description"]
+
+            if description.downcase.include? assetname
+              noresults = false
+              puts element["_source"]["title"]
+              puts "\n"
+
+            end
+
+          end
+
+          if noresults == true
+            puts "No Results"
+            puts "\n"
+          end
+
+        end
+
+      end
+
+      puts "============="
+
+    end
+
+    #searches json queries for presence of an asset, will update description with asset if found in the query.
+    # output is a new json file
+    def update_descriptions
+      #create output array so that multiple objects can be imported back into kibana.
+      outputarray = Array.new
+
+      puts "============== \n"
+
+      obj = JSON.parse(File.read('multiplenodesc.json'))
+      obj.each do |element|
+
+        puts element["_source"]["title"]
+
+        File.open('assetlist.txt') do |searchlist|
+          searchlist.each_line do |searchquery|
+
+            puts "Asset: " + searchquery
+            assetnameasinput = searchquery.chomp
+            assetname = searchquery.downcase.chomp
+
+            #this may only be for saved searches, will check later with visualisations
+            searchqueryJSON = JSON.parse(element["_source"]["kibanaSavedObjectMeta"]["searchSourceJSON"])
+
+            querystring = searchqueryJSON["query"]["query_string"]["query"]
+            querystring = querystring.downcase.chomp
+
+            if querystring.include? assetname
+
+              puts "found assetname"
+
+              #add to description if it is doesn't already include the asset
+              description = element["_source"]["description"]
+
+              if description.downcase.include? assetname
+                #do nothing
+              else
+                description = description + " " + assetnameasinput
+                puts "add to description"
+              end
+
+              element["_source"]["description"] = description
+
+              puts element["_source"]["description"]
+
+            end
+          end
+        end
+
+        #elements are put back into an array so they can be read by kibana.
+        outputarray << element
+
+        puts "Writing updated search to output file"
+        File.open('output.json', 'w') do |f|
+          f.write(JSON.pretty_generate(outputarray))
+        end
+      end
+
+      puts "============="
+
     end
 
   end
 
-  puts "============="
-
 end
+
+
 
 
---
+++ lib/thisistesting.rb
@@ -0,0 +1,23 @@
+
+class Thisistesting < StandardError;
+
+require 'rubygems'
+require 'json'
+require 'pp'
+require './savedsearchsorter'
+
+include Savedsearchsorter
+
+
+  testzone = Utilitymethods.new()
+
+  #testzone.jsonparsetest
+
+  #testzone.userinputtest
+  #
+  testzone.update_descriptions
+
+
+
+end
+
